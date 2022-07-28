0721ec16e5bb16bc328c61dcd2f438916ba2e59b
air_user <shapovalovasvitlana@gmail.com>
2022-07-28 00:14:56 -0700

    lesson_2

---
+++ python_lesson3/dialog_program.py
@@ -0,0 +1,11 @@
+programme = input("Hello, my name is Jarvis. What is your name?:")
+customer = str(programme)
+
+print("Hello,", customer)
+programme_2 = input("Think of a number:")
+customer_2 = int(programme_2)
+
+programme_3 = input("Think of another number:")
+customer_3 = int(programme_3)
+programme_4 = customer_2 + customer_3
+print(customer_2, '+', customer_3, "=", programme_4)
