d4586271fc071754d8c8bed3641aeaa4b14e3b39
air_user <shapovalovasvitlana@gmail.com>
2022-07-28 00:14:22 -0700

    lesson_2

---
+++ work_print.py
@@ -0,0 +1,16 @@
+print("Good day")
+print("Good", "day - 2", "!")
+print("Good", "day - 2", "!", sep="->")
+print("Good", "morning", end="?", sep=" 123 ")
+print("fox")
+print("window")
+
+my_best_scores = (10 + 10) / 2
+print(my_best_scores)
+
+integer = input("insert a number:")
+var = int(integer)
+print(var + 12)
+None
+
+
